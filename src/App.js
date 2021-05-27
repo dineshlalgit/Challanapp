eccfe2ad51cc56547032ce6d3cef7c1891bafd0a
dineshlal <dineshlal29@gmail.com>
2021-05-27 19:22:44 +0530

    updated aws login support

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Map from "./componentMap/Map"
 import './App.css';
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+import { withAuthenticator,AmplifySignOut} from '@aws-amplify/ui-react'
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
     <div className="App">
+      <AmplifySignOut />
       <h1>Challan Wala</h1>
+      <p>sourab ka gand</p>
       <Map />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
